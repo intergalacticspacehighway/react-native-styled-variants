34d7d7e317a1c6f128b828954c6daeabc6251a27
Nishan <nishanbende@gmail.com>
2021-09-21 04:12:03 +0530

    feat: sx fn

--- babel-plugin/index.js
+++ babel-plugin/index.js
@@ -1,14 +1,16 @@
 const visitor = require('./visitor');
-const utilityPropVisitor = require('./utility-prop-visitor');
+const utilityPropVisitor = require('./utility-prop-v2');
 
 function transformer() {
   return {
     visitor: {
       ...visitor,
-      ...utilityPropVisitor,
-      Program(path) {
+      'Program'(path) {
         visitor.Program(path);
         utilityPropVisitor.Program(path);
+      },
+      'FunctionDeclaration|ArrowFunctionExpression'(path) {
+        utilityPropVisitor['FunctionDeclaration|ArrowFunctionExpression'](path);
       },
     },
   };
--- babel-plugin/playground.js
+++ babel-plugin/playground.js
@@ -2,7 +2,7 @@ const { parse } = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const generate = require('@babel/generator').default;
 const visitor = require('./visitor');
-const utilityPropVisitor = require('./utility-prop-visitor');
+const utilityPropVisitor = require('./utility-prop-v2');
 
 function transformToStyles(code) {
   const ast = parse(code, {
@@ -12,10 +12,12 @@ function transformToStyles(code) {
 
   traverse(ast, {
     ...visitor,
-    ...utilityPropVisitor,
-    Program(path) {
+    'Program'(path) {
       visitor.Program(path);
       utilityPropVisitor.Program(path);
+    },
+    'FunctionDeclaration|ArrowFunctionExpression'(path) {
+      utilityPropVisitor['FunctionDeclaration|ArrowFunctionExpression'](path);
     },
   });
 
@@ -25,8 +27,22 @@ function transformToStyles(code) {
 }
 
 transformToStyles(`
-const Button = createVariant(Pressable, {
-  backgroundColor: "$colors.red",
-});
+const shadow = {x: 1}
+export default () => {
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <Container>
+          <View style={sx({ flex: 1, ...shadow })}>
+            <View style={sx({ flex: 2 })}>
+              <Header />
+            </View>
+            <Screen />
+          </View>
+        </Container>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+};
 
 `);
